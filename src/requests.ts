ea09e27c3a25df574eacc886bc37d428735e08c2
John Evans <38507954+King-Mob@users.noreply.github.com>
2026-06-24 16:55:11 +0100

    update env so doesn't clash

--- src/requests.ts
+++ src/requests.ts
@@ -1,7 +1,7 @@
-const { secret, wrapper_url } = process.env;
+const { events_secret, wrapper_url } = process.env;
 
 export async function sendMessage(roomId: string, message: string) {
-    return fetch(`${wrapper_url}/api/send?roomId=${roomId}&toolId=events&secret=${secret}`, {
+    return fetch(`${wrapper_url}/api/send?roomId=${roomId}&toolId=events&secret=${events_secret}`, {
         method: "POST",
         body: JSON.stringify({
             message
